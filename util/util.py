f8346e1b68ba5d03651b43594f581f7c69f2b1d1
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2018-05-31 18:13:07 +0300

    Create util folder and move util.py there

---
+++ util/util.py
@@ -0,0 +1,12 @@
+"""
+Utilities for Hive runtimes.
+"""
+
+from datetime import datetime
+
+def printt(string):
+    '''
+    print function that always prefixes current datetime.
+    '''
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+    print "[%s] %s" % (now, string)
